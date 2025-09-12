eed50174e3d7b1907816380e419d1db53f6c2d70
Haya Lakhani <157073204+lakhani-haya@users.noreply.github.com>
2025-09-11 23:32:19 -0500

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -25,7 +25,7 @@ echo 📦 Installing dependencies...
 pip install -r requirements.txt
 
 echo.
-echo 🎉 Setup complete! 🎉
+echo Setup complete! 🎉
 echo.
 echo To run the app:
 echo 1. Activate the environment:
